4f3e5c755e70a02af1a06325af15574cfb71c2cf
vduernberger <100835083+vduernberger@users.noreply.github.com>
2022-06-13 00:01:58 +0200

    Kinderaddierer - 1 Level

---
+++ MyGame/Scripts/SoRuntimeData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SoRuntimeData")]
+public class SoRuntimeData : ScriptableObject
+{
+    public string nameKid = "";
+    public string showPic = "";
+    public string scoreResult = "";
+    public int scoreMessage;
+
+    private void OnEnable()
+    {
+    }
+}
